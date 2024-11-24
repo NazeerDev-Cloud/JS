8c68935f1daff31f417055511586df1e6092c061
Nazeer Ahmad <nazeerdev.official@gmail.com>
2024-11-24 23:45:17 +0000

    conversion of datatypes

---
+++ Basics/datatypes.js
@@ -0,0 +1,12 @@
+"use strict"
+console.log("Datatypes")
+
+let name = "Dummy";
+const age  = null;
+
+
+console.log(typeof age);
+console.log(typeof (age));
+let valueInNUmber = Number(age)
+console.log(typeof valueInNUmber)
+console.log(valueInNUmber)
---
+++ Basics/variables.js
@@ -0,0 +1,9 @@
+const accountId = 123456;
+let accountEmail = "bunny@gmail.com"
+var accountPassword = "0123"
+accountCity = "Islamabad";
+let accountState;
+
+// accountId = 24;
+ console.log(accountId);
+ console.table([accountId , accountEmail , accountPassword , accountCity , accountState])
