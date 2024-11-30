f5392bab74e01c3e08221cb73d4c46ef0e06f593
Nazeer Ahmad <nazeerdev.official@gmail.com>
2024-11-30 01:06:00 +0000

    strings

--- Basics/datatypes.js
+++ Basics/datatypes.js
@@ -1,12 +1,50 @@
 "use strict"
-console.log("Datatypes")
-
-let name = "Dummy";
-const age  = null;
+// console.log("Datatypes")
 
 
-console.log(typeof age);
-console.log(typeof (age));
-let valueInNUmber = Number(age)
-console.log(typeof valueInNUmber)
-console.log(valueInNUmber)
+// Datatypes :
+// A- Premitive Datatypes  (Call  for value):
+// 1- BigInt
+// 2- String  
+// 3- Number
+// 4- Null
+// 5- Undefined
+// 6-Boolean (True/ False)
+// 7- Symbol
+
+// B- Reference Type (Non-Premiticves Datatypes)
+// 1- Arrays 
+// 2- Objects
+// 3- Functions 
+
+// let name = "Dummy";
+// const age  = null;
+
+
+// console.log(typeof age);
+// console.log(typeof (age));
+// let valueInNUmber = Number(age)
+// console.log(typeof valueInNUmber)
+// console.log(valueInNUmber) 
+// console.group("Type Script")
+// const isLoggedIn = false
+// const outSideTemp = null
+// let userEmail;
+// const id = Symbol('123')
+// const myId = Symbol('123')
+// console.log(id) 
+// console.log(myId)
+// console.log(id === myId)
+
+const value = ["C1"," C2" , "C2"];
+const myObj = {
+    name : "nazeer",
+    age : "23" ,
+    id: 123
+
+}
+ const myFuntion = function(){
+    console.log("TypeScript Java Script");
+    
+ }
+ console.log(typeof myFuntion)
---
+++ Basics/strings.js
@@ -0,0 +1,14 @@
+let someNumber =  33
+ let stringNumber = String(someNumber)
+ console.log(stringNumber)
+ console.log(typeof stringNumber)
+
+ let value = 3
+ let negvalue = -value
+ console.log(negvalue)
+
+ let gameCounter = 100;
+ gameCounter++;
+ console.log(gameCounter)
+
+ 
