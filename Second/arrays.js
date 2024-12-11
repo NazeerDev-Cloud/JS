349cda12e559983b08b1addd57a02a48c22fad07
Nazeer Ahmad <nazeerdev.official@gmail.com>
2024-12-11 21:30:30 +0000

    Arrays in JavaScript

--- Second/arrays.js
+++ Second/arrays.js
@@ -2,3 +2,32 @@
 
 let arr = [10,20,30,40,50,"Nazeer"]
 console.log(arr);
+
+
+const arr2 = ["Name" , "Age" , "Phone" , "Gender" , "Location"]
+//  name = "Nazeer",
+// age = 22,
+// phone = "03030130907",
+// gender = "Male",
+// Location = "Rawalpindi"
+console.log(arr2); 
+
+// Methods in Arrays
+
+const myarr = [1,2,3,4,5]
+console.log(myarr);
+myarr.push(6)
+console.log(myarr);
+myarr.pop()
+console.log(myarr);
+myarr.unshift(9)
+console.log(myarr);
+myarr.shift(7)
+console.log(myarr);
+
+
+
+
+
+
+
